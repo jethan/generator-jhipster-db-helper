2f1ae7a985c76cf27723001da8ceebbe4a59a597
Adrien Horgnies <aHorgnies@altissia.com>
2017-05-09 15:33:05 +0200

    removed lots of ?useless? stuff

--- travis/install/install-jhipster-stack.sh
+++ travis/install/install-jhipster-stack.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-set -ev
-#-------------------------------------------------------------------------------
-# Update npm
-#-------------------------------------------------------------------------------
-#npm install -g npm
-#-------------------------------------------------------------------------------
-# Install yeoman, bower and gulp
-#-------------------------------------------------------------------------------
-#yarn global install yo
-#yarn global install bower
-#yarn global install gulp-cli
-#yarn global install generator-jhipster
-#-------------------------------------------------------------------------------
-# Install the latest version of JHipster
-#-------------------------------------------------------------------------------
-#cd "$TRAVIS_BUILD_DIR"/
-#yarn install
-echo -e "$JHIPSTER_TRAVIS \n $JHIPSTER_INSTALL \n $JHIPSTER_SAMPLES \n $JHIPSTER_SCRIPTS"
 yarn link
-echo "$(pwd) contains the application"
-ls
